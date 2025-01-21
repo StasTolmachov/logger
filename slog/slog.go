28c692c3d0ac0ea3a8d6d3e1d63abd3413df3b9e
10493 <stas@tolmachov.dev>
2025-01-21 08:08:19 +0100

    add slog

--- slog/slog.go
+++ slog/slog.go
@@ -1,70 +1,97 @@
-// NOTE: Not well tested, just an illustration of what's possible
-package main
+package logger
 
 import (
 	"context"
 	"encoding/json"
+	"github.com/google/uuid"
 	"io"
 	"log"
 	"log/slog"
 	"os"
+	"path/filepath"
 	"runtime"
 	"time"
 
 	"github.com/fatih/color"
 )
 
+// PrettyHandlerOptions contains options specific to the PrettyHandler, mainly around slog handling.
 type PrettyHandlerOptions struct {
 	SlogOpts slog.HandlerOptions
 }
 
+// PrettyHandler implements slog.Handler and provides a structured, colored logging output.
 type PrettyHandler struct {
 	slog.Handler
 	l *log.Logger
 }
 
+// Log is a global logger instance used across the application.
+var Log *slog.Logger
+
+// Handle processes a single log record, formats it, and outputs it to the configured io.Writer.
 func (h *PrettyHandler) Handle(ctx context.Context, r slog.Record) error {
-	level := r.Level.String() + ":"
+	// Change color based on log level
+	level := r.Level.String()
 
 	switch r.Level {
 	case slog.LevelDebug:
 		level = color.MagentaString(level)
 	case slog.LevelInfo:
-		level = color.BlueString(level)
+		level = color.BlueString(level + " ")
 	case slog.LevelWarn:
-		level = color.YellowString(level)
+		level = color.YellowString(level + " ")
 	case slog.LevelError:
 		level = color.RedString(level)
 	}
 
+	// Collect log attributes
 	fields := make(map[string]interface{}, r.NumAttrs())
-	r.Attrs(func(a slog.Attr) bool {
-		fields[a.Key] = a.Value.Any()
 
+	r.Attrs(func(a slog.Attr) bool {
+		if a.Key == "err" && a.Value.Any() != nil {
+			err, ok := a.Value.Any().(error)
+			if ok {
+				fields[a.Key] = err.Error()
+			} else {
+				fields[a.Key] = a.Value.Any()
+			}
+		} else {
+			fields[a.Key] = a.Value.Any()
+		}
 		return true
 	})
 
+	// Capture the source from runtime call stack
+	source := make(map[string]interface{}, r.NumAttrs())
+
+	fs := runtime.CallersFrames([]uintptr{r.PC})
+	frame, _ := fs.Next()
+	source["file"] = filepath.Base(frame.File)
+	source["line"] = frame.Line
+	source["func"] = color.CyanString(frame.Function)
+
+	// Format the timestamp
+	timeStr := color.GreenString(r.Time.Format(time.DateTime))
+	msg := r.Message
+
+	// Check for a trace ID in the context and add it to the log fields if present
+	traceID, ok := ctx.Value("trace-id").(uuid.UUID)
+	if ok {
+		fields["trace-id"] = traceID
+	}
 	b, err := json.MarshalIndent(fields, "", "  ")
 	if err != nil {
 		return err
 	}
 
-	timeStr := r.Time.Format(time.DateTime)
-	msg := color.CyanString(r.Message)
-
-	// Извлечение источника (имя файла, строка, функция)
-	source := ""
-	if r.PC != 0 {
-		fs := runtime.CallersFrames([]uintptr{r.PC})
-		frame, _ := fs.Next()
-		source = color.GreenString("%s:%d (%s)", frame.File, frame.Line, frame.Function)
-	}
-
-	h.l.Println(timeStr, level, msg, source, string(b))
+	// Print the formatted log entry
+	h.l.Printf("%v | %v | %v | %v | %v:%v %v", timeStr, level, msg, source["func"], source["file"], source["line"], string(b))
 
 	return nil
 }
 
+// NewPrettyHandler creates a new PrettyHandler with a given output writer and options.
 func NewPrettyHandler(
 	out io.Writer,
 	opts PrettyHandlerOptions,
@@ -76,31 +103,16 @@ func NewPrettyHandler(
 
 	return h
 }
-func main() {
 
-	var err error
+// MakeLogger initializes and configures the global logger instance.
+func MakeLogger() {
 	opts := PrettyHandlerOptions{
 		SlogOpts: slog.HandlerOptions{
-			AddSource: true,
 			Level:     slog.LevelDebug,
+			AddSource: true,
 		},
 	}
+
 	handler := NewPrettyHandler(os.Stdout, opts)
-	logger := slog.New(handler)
-
-	logger.Debug(
-		"executing database query",
-		slog.String("query", "SELECT * FROM users"),
-	)
-	logger.Info("image upload successful", slog.String("image_id", "39ud88"))
-	logger.Warn(
-		"storage is 90% full",
-		slog.String("available_space", "900.1 MB"),
-	)
-	logger.Error(
-		"An error occurred while processing the request",
-		slog.String("url", "https://example.com"),
-	)
-
-	logger.Error("err", "errkey", err)
+	Log = slog.New(handler)
 }
